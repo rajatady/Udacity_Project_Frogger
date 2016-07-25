a851e07788a97b9864d32d1801816a579bd4e3fc
Kumar DIvya Rajat <rajat.ady@gmail.com>
2016-07-25 14:48:25 +0530

    Minor changes

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
+    this.x = 0;
+    this.y = 0;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -25,11 +27,36 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function () {
+    this.sprite = "images/char-boy.png";
+    this.x = ctx.width/2;
+    this.y = ctx.height/2;
+};
+
+Player.prototype.update  = function (dt,x,y) {
+    this.x = dt * x;
+    this.y = dt * y;
+};
+
+Player.prototype.handleInput  = function (keyCode) {
+    // this.x = dt * x;
+    // this.y = dt * y;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+for(var i = 0 ; i < 10; i++) {
+    allEnemies.push(new Enemy());
+}
+
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
